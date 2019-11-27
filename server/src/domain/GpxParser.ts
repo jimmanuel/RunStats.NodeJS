6794bf52b9a819a55817361b2b3eb1a534eedad4
jimmanuel <jimmanuel@gmail.com>
2019-11-27 14:40:56 -0500

    promisified the gpx helper

--- server/src/domain/GpxParser.ts
+++ server/src/domain/GpxParser.ts
@@ -5,28 +5,21 @@ import { DateTimeHelper } from './DateTimeHelper';
 
 export class GpxParser {
 
-    static parseGpx(xmlData: string, callback:(activity : RunActivity, error: Error) => void) : void {
+    public static async parseGpx(xmlData: string) : Promise<RunActivity> {
 
         let parser = new xml2js.Parser();
+        
+        let result = await parser.parseStringPromise(xmlData); 
 
-        parser.parseString(xmlData, (err, result) => {
-            if (!(err === null)) {
-                callback(null, err);
-                return;
-            }
+        let timeValue = result.gpx.trk[0].time[0];
 
-            let timeValue = result.gpx.trk[0].time[0];
+        let dataPoints : Array<DataPoint> = new Array();
+        let jsonPoints = result.gpx.trk[0].trkseg[0].trkpt;
+        for(let dataPointIndex = 0; dataPointIndex < jsonPoints.length; dataPointIndex++) {
+            let item = jsonPoints[dataPointIndex];
+            dataPoints.push(new DataPoint(item.$.lat, item.$.lon, item.ele[0], DateTimeHelper.convertToEpoch(item.time[0])));                
+        }
 
-            let dataPoints : Array<DataPoint> = new Array();
-            let jsonPoints = result.gpx.trk[0].trkseg[0].trkpt;
-            for(let dataPointIndex = 0; dataPointIndex < jsonPoints.length; dataPointIndex++) {
-                let item = jsonPoints[dataPointIndex];
-                dataPoints.push(new DataPoint(item.$.lat, item.$.lon, item.ele[0], DateTimeHelper.convertToEpoch(item.time[0])));                
-            }
-
-            callback(
-                new RunActivity(DateTimeHelper.convertToEpoch(timeValue), dataPoints), 
-                null);
-        });
+        return new RunActivity(DateTimeHelper.convertToEpoch(timeValue), dataPoints);
     }
 }
--- server/test/domain/GpxParser.spec.ts
+++ server/test/domain/GpxParser.spec.ts
@@ -8,42 +8,37 @@ import { setTimeout } from 'timers';
 
 describe('GpxParser', () => {
   describe("parseGpx", () => {
-    it('should load file one correctly', (done) => {
-		setTimeout(done, 500);
+    it('should load file one correctly', async () => {
         let content = fs.readFileSync(__dirname + '/../../../test/res/RK_gpx_2011-10-18_0635PM.gpx').toString();
 		
-        GpxParser.parseGpx(content, (activity : RunActivity, error : Error) => {
-			expect(error).to.be.equal(null, 'there should be no error generated');
-			expect(activity).to.not.equal(null, 'a valid activity should be returned');
-			expect(activity.epochStartTime).to.equal(1318962941, 'the time should be correct');
-			expect(activity.dataPoints).to.not.equal(null, 'there should be data points');
-			expect(activity.dataPoints.length).to.equal(205, 'the number of data points should be correct')
-		});
+        let activity = await GpxParser.parseGpx(content);
+		expect(activity).to.not.equal(null, 'a valid activity should be returned');
+		expect(activity.epochStartTime).to.equal(1318962941, 'the time should be correct');
+		expect(activity.dataPoints).to.not.equal(null, 'there should be data points');
+		expect(activity.dataPoints.length).to.equal(205, 'the number of data points should be correct')
+
       });
 	
-      it('should load file two correctly', (done) => {
-		setTimeout(done, 500);
+      it('should load file two correctly', async () => {
         let content = fs.readFileSync(__dirname + '/../../../test/res/RK_gpx _2012-04-26_0704.gpx').toString();
 		
-		GpxParser.parseGpx(content, (activity : RunActivity, error : Error) => {
-			expect(error).to.equal(null, 'there should be no error generated');
-			expect(activity).to.not.equal(null, 'a valid activity should be returned');
-			expect(activity.epochStartTime).to.equal(1335431097, 'the time should be correct');
-			expect(activity.dataPoints).to.not.equal(null, 'there should be data points');
-			expect(activity.dataPoints.length).to.equal(160, 'the number of data points should be correct')
+		let activity = await GpxParser.parseGpx(content);
+		expect(activity).to.not.equal(null, 'a valid activity should be returned');
+		expect(activity.epochStartTime).to.equal(1335431097, 'the time should be correct');
+		expect(activity.dataPoints).to.not.equal(null, 'there should be data points');
+		expect(activity.dataPoints.length).to.equal(160, 'the number of data points should be correct')
 
-			expect(activity.dataPoints[0].altitude).to.equal('73.2', 'first elevation should be correct');
-			expect(activity.dataPoints[0].latitude).to.equal('38.888073000', 'first latitude should be correct');
-			expect(activity.dataPoints[0].longitude).to.equal('-77.088644000', 'first longitude should be correct');
+		expect(activity.dataPoints[0].altitude).to.equal('73.2', 'first elevation should be correct');
+		expect(activity.dataPoints[0].latitude).to.equal('38.888073000', 'first latitude should be correct');
+		expect(activity.dataPoints[0].longitude).to.equal('-77.088644000', 'first longitude should be correct');
 
-			expect(activity.dataPoints[87].altitude).to.equal('83.7', 'middle elevation should be correct');
-			expect(activity.dataPoints[87].latitude).to.equal('38.881250000', 'middle latitude should be correct');
-			expect(activity.dataPoints[87].longitude).to.equal('-77.098731000', 'middle longitude should be correct');
+		expect(activity.dataPoints[87].altitude).to.equal('83.7', 'middle elevation should be correct');
+		expect(activity.dataPoints[87].latitude).to.equal('38.881250000', 'middle latitude should be correct');
+		expect(activity.dataPoints[87].longitude).to.equal('-77.098731000', 'middle longitude should be correct');
 
-			expect(activity.dataPoints[153].altitude).to.equal('76.3', 'last elevation should be correct');
-			expect(activity.dataPoints[153].latitude).to.equal('38.886893000', 'last latitude should be correct');
-			expect(activity.dataPoints[153].longitude).to.equal('-77.089452000', 'last longitude should be correct');
-		});
+		expect(activity.dataPoints[153].altitude).to.equal('76.3', 'last elevation should be correct');
+		expect(activity.dataPoints[153].latitude).to.equal('38.886893000', 'last latitude should be correct');
+		expect(activity.dataPoints[153].longitude).to.equal('-77.089452000', 'last longitude should be correct');
 	  });
   });
 });
