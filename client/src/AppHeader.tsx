3c45ad5e1849c7850b7dbba016bea623b44e0c8b
Jim <jimmanuel@gmail.com>
2019-12-11 07:18:46 -0500

    added a file picker for GPX files

--- client/src/AppHeader.tsx
+++ client/src/AppHeader.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 import './AppHeader.css';
 
 class AppHeader extends React.Component {
 
     private async importFiles() : Promise<void> {
-      alert('hello there');
+      if (!this.inputOpenFileRef.current) {
+        console.log(`it's null so I'm returning early`)
+        return;
+      }
+      await this.inputOpenFileRef.current.click();
+      const fileList: FileList = this.inputOpenFileRef.current.files ? this.inputOpenFileRef.current.files : new FileList();
+      for(let i = 0; i < fileList.length; i++) {
+        
+        let file = fileList.item(i);
+        if (!file) {
+          console.log(`file at ${i} is null`)
+          continue;
+        }
+        console.log(file.name);
+
+        
+      }
     }
   
     render() {
@@ -15,14 +31,20 @@ class AppHeader extends React.Component {
         </div>
         <div className="Button-Table">
           <div className="Button-container">
+          <input ref={this.inputOpenFileRef} type="file" accept=".gpx" multiple style={{display:"none"}}/>
             <button className="Import-button" onClick={() => this.importFiles()}>Import File(s)</button>
           </div>
         </div>
       </div>)
     }
+
+
+    private readonly inputOpenFileRef : RefObject<HTMLInputElement>
   
     constructor(props: any) {
       super(props);
+
+      this.inputOpenFileRef = React.createRef()
     }
   }
   
