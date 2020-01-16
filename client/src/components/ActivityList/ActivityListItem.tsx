ebe26059c36b1a06804feade22bd02ffb3e1bd48
jimmanuel <jimmanuel@gmail.com>
2020-01-16 07:05:58 -0500

    found 5 minutes to fix the distance on the header

--- client/src/components/ActivityList/ActivityListItem.tsx
+++ client/src/components/ActivityList/ActivityListItem.tsx
@@ -10,6 +10,32 @@ export interface IActivityListItemProps {
 export class ActivityListItem extends React.Component<IActivityListItemProps, any> {
 
 
+	// private static secondsToString(sec : number) : string 
+	// {
+	// 	const secPerHour = 60 * 60;
+		
+	// 	const hours = sec / secPerHour;
+		
+	// 	sec = (sec - (hours * secPerHour));
+		
+	// 	const minutes = sec / 60;
+	// 	sec = (sec - (minutes * 60));
+		
+	// 	if (hours > 0)
+	// 		return String.format("%d:%02d:%02d", hours, minutes, sec);
+	// 	return String.format("%02d:%02d", minutes, sec);
+	// }
+
+    private static round(x: number, precision: number) : number {
+        const p = Math.pow(10, precision-1);
+        return Math.round(x * p) / p;
+    }
+
+	private static metersToMiles(meters : number) : number {
+		return meters * 0.000621371192;
+	}
+	
+
     private getReadableStartTime() : string {
         let startDate = new Date(0);
         startDate.setUTCSeconds(this.props.activity.epochStartTime);
@@ -17,8 +43,7 @@ export class ActivityListItem extends React.Component<IActivityListItemProps, an
     }
 
     private getReadableDistance() : string {
-        
-        return '(Distance)';
+        return `${ActivityListItem.round(ActivityListItem.metersToMiles(this.props.activity.distanceMeters), 3)} miles`;
     }
 
     public handleClick() : void {
