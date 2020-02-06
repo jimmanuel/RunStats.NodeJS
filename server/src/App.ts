be94f54cde129bf826a312079d48fad610363327
jimmanuel <jimmanuel@gmail.com>
2020-02-06 05:59:59 -0500

    added a better health check method

--- server/src/App.ts
+++ server/src/App.ts
@@ -64,7 +64,7 @@ class App {
         router.options('*');
         router.post('/api/user/googletoken', genericTextHanlder, (req, res) => this.authRouter.loginGoogle(req, res));
         router.get('/api/config', (req, res) => this.configRouter.getConfig(req, res));
-        router.get('/api/isonline', (req, res) => res.send(200).end());
+        router.get('/api/health', (req, res) => res.send(200).end());
         router.get('/*', express.static(__dirname + '/client'));
 
         this.express.use('/', router);
